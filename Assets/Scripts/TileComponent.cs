86e15e993e3d437e530610b243cd7822a0f3fb8f
AngelGamesS <95039976+AngelGamesS@users.noreply.github.com>
2023-07-16 16:33:05 +0300

    imports and such

---
+++ Assets/Scripts/TileComponent.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileComponent : MonoBehaviour
+{
+    public TileType type;
+    public bool placeable;
+    private bool towerWasPlaced;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        towerWasPlaced = false;
+        if(type == TileType.EnemyPath){
+            placeable = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum TileType{
+    None,
+    Water,
+    Grass,
+    EnemyPath
+}
